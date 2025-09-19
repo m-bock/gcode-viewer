5808b309ebd22cc581aca751968d281202608908
Michael Bock <no-day@posteo.net>
2025-09-19 10:43:47 +0200

    Add Ticks component with styles and export

---
+++ src/Ticks/Ticks.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Ticks.module.css';
+
+export const Ticks: React.FC = () => {
+    return (
+        <div className={styles.kjkjk}>
+            {/* Ticks component implementation will go here */}
+        </div>
+    );
+};
